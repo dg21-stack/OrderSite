c24879f8d6b847072db92f798bb38e0d9ccea8ed
Daniel Gaidar <78220012+dg21-stack@users.noreply.github.com>
2022-08-12 21:55:37 -0400

    Delete foodSite directory

--- foodSite/components/Card/DetailCard.js
+++
@@ -1,7 +0,0 @@
-import classes from './Card.module.css';
-
-const DetailCard = (props) => {
-    return <div className = {classes.detailCard}>{props.children}</div>
-}
-
-export default DetailCard;
--- foodSite/components/Card/ItemCard.js
+++
@@ -1,7 +0,0 @@
-import classes from './Card.module.css';
-
-const ItemCard = (props) => {
-    return <div className = {classes.itemCard}>{props.children}</div>
-}
-
-export default ItemCard;
